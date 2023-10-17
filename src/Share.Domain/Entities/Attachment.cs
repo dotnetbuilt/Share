b660608dd66ac72655b2d816bf8d1b4f350f5117
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-17 23:01:11 +0500

    Add Attachment

---
+++ src/Share.Domain/Entities/Attachment.cs
@@ -0,0 +1,8 @@
+namespace Share.Domain.Entities;
+
+public class Attachment
+{
+    public long Id { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+}
