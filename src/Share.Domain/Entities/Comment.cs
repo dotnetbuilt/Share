1916be09ec20fa4e66d520bccd9a679724529230
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-17 23:20:02 +0500

    Add Comment

---
+++ src/Share.Domain/Entities/Comment.cs
@@ -0,0 +1,14 @@
+using Share.Domain.Commons;
+
+namespace Share.Domain.Entities;
+
+public class Comment:Auditable
+{
+    public string Text { get; set; }
+
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+    public long StoryId { get; set; }
+    public Story Story { get; set; }
+}
