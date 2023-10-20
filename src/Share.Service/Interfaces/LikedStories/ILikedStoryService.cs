76a3ebc9e81258242734a9f2c1aa37222394f90d
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-21 00:09:08 +0500

    Add ILikedStoryService

---
+++ src/Share.Service/Interfaces/LikedStories/ILikedStoryService.cs
@@ -0,0 +1,12 @@
+using Share.Service.DTOs.LikedStories;
+
+namespace Share.Service.Interfaces.LikedStories;
+
+public interface ILikedStoryService
+{
+    ValueTask<LikedStoryResultDto> AddAsync(LikedStoryCreationDto dto);
+    ValueTask<bool> RemoveAsync(long likedStoryId);
+    ValueTask<IEnumerable<LikedStoryResultDto>> RetrieveAllByUserIdAsync(long userId);
+    ValueTask<IEnumerable<LikedStoryResultDto>> RetrieveAllByStoryIdAsync(long storyId);
+    ValueTask<long> RetrieveNumberOfLikesByStoryIdAsync(long storyId);
+}
