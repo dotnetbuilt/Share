94a2bd1a99beb337a9cf3c68aaff8a9309ca95af
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-20 14:43:49 +0500

    Add IUserService

---
+++ src/Share.Service/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+using Share.Service.DTOs.Users;
+
+namespace Share.Service.Interfaces;
+
+public interface IUserService
+{
+    ValueTask<UserResultDto> AddAsync(UserCreationDto dto);
+    ValueTask<UserUpdateDto> ModifyAsync(UserUpdateDto dto);
+    ValueTask<bool> RemoveAsync(long userId);
+    ValueTask<UserResultDto> RetrieveByIdAsync(long userId);
+    ValueTask<IEnumerable<UserResultDto>> RetrieveAllAsync();
+    ValueTask<long> RetrieveNumberOfUsers();
+}
